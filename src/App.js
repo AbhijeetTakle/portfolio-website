061ad86f2c7ee6ec706ed70d3489016386c2e872
Abhijeet Takle <abhijeettakle35@gmail.com>
2022-12-12 17:08:57 +0530

    header component added,  header title and image also added and social media strip also added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import SocialMediaStrip from "./components/socialmedia/SocialMediaStrip";
 
 function App() {
   return (
     <div className="App">
+      <SocialMediaStrip />
       <Header />
     </div>
   );
