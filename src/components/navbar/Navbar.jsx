a328a2040bfee4544646fa5ff43022893d362c6f
Abhijeet Takle <abhijeettakle35@gmail.com>
2022-12-13 13:19:59 +0530

    Nav Bar Component ready to roll add logo and button styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,45 @@
 import React from "react";
 import "./Navbar.css";
+import Logo from "../../assets/images/A3.png";
 const Navbar = () => {
   return (
     <div className="navbar-container">
-      <div className="main-logo"></div>
+      <div className="main-logo-container">
+        <img className="main-logo" src={Logo} alt="Abhijeet Nana Takle" />
+      </div>
       <div className="nav-links-container">
         <ul className="nav-links">
-          <li className="nav-link">About</li>
-          <li className="nav-link">Projects</li>
-          <li className="nav-link">Skills</li>
-          <li className="nav-link">Education</li>
-          <li className="nav-link">Work ex</li>
-          <li className="nav-link">Contacts</li>
+          <li className="nav-link">
+            <div className="nav-link-text">About</div>
+            <span className="underline-effect"></span>
+          </li>
+          <li className="nav-link">
+            <div className="nav-link-text">Projects</div>
+            <span className="underline-effect"></span>
+          </li>
+          <li className="nav-link">
+            <div className="nav-link-text">Skills</div>
+            <span className="underline-effect"></span>
+          </li>
+          <li className="nav-link">
+            <div className="nav-link-text">Education</div>
+            <span className="underline-effect"></span>
+          </li>
+          <li className="nav-link">
+            <div className="nav-link-text">Work Ex</div>
+            <span className="underline-effect"></span>
+          </li>
+          <li className="nav-link">
+            <div className="nav-link-text">Contacts</div>
+            <span className="underline-effect"></span>
+          </li>
         </ul>
       </div>
-      <div className="nav-extra"></div>
+      <div className="nav-extra">
+        <div className="login-btn">
+          <span className="login-text">Log In</span>
+        </div>
+      </div>
     </div>
   );
 };
