d1aff0bb8e9aae03cd1264999f3f95eccda4afb8
Abhijeet Takle <abhijeettakle35@gmail.com>
2022-12-31 15:58:56 +0530

    renewed navbar with new font and style

---
+++ src/components/workex/Workex.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Workex.css";
+
+const Workex = () => {
+  return (
+    <div className="workex-container" id="work">
+      <div className="workex-timeline-container">
+        <div className="workex-timeline"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Workex;
