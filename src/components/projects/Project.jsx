ca893a39cf4038bbb1f7874452027292216a582b
Abhijeet Takle <abhijeettakle35@gmail.com>
2023-01-03 03:45:11 +0530

    modified project grid and made it modern

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,39 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ProjectLinkButton from "./ProjectLinkButton";
 import "./Project.css";
+import { FaArrowAltCircleRight } from "react-icons/fa";
 
 const Project = (props) => {
+  const floatinWindowToggle = (e) => {
+    e.preventDefault();
+    const classwin = ".win-" + e.target.classList[1];
+    const projectwindow = document.querySelector(classwin);
+    projectwindow.style.display = "block";
+  };
+
   return (
     <section className="project-container">
-      <div className="project-info-container">
-        <div className="project-title-container">
-          <h3 className="project-title">{props.projectTitle}</h3>
+      {props.comingSoon === true ? (
+        <div className={"coming-soon " + props.projectNumber + "cs"}>
+          <p>coming soon!</p>
         </div>
+      ) : (
+        ""
+      )}
+      <div className="project-info-container">
         <div className="project-description-container">
           <p className="project-description">{props.projectDescription}</p>
         </div>
-        <div className="project-links-container">
-          <ProjectLinkButton
-            link_type="demo-btn"
-            link_string={props.projectDemoLink}
-            link_text="Live"
-          />
-          <ProjectLinkButton
-            link_type="code-btn"
-            link_string={props.projectCodeLink}
-            link_text="Source"
-          />
-        </div>
-      </div>
-      <div className="project-video-container">
-        <div className="project-video">
-          <iframe
-            src="https://www.youtube.com/embed/xNRJwmlRBNU"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+        <div className="project-title-container">
+          <a
+            href={props.projectLiveLink}
+            target="_blank"
+            className="project-title"
+          >
+            {props.projectTitle}
+          </a>
+          <a className="arrow-project ">
+            <FaArrowAltCircleRight />
+            <div
+              className={"click-div " + props.projectNumber}
+              onClick={floatinWindowToggle}
+            ></div>
+          </a>
         </div>
       </div>
     </section>
